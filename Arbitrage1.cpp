09644fbd55b1ab5a1db2eaa3c4d8bb73b7fff5bb
DBQ1618 <divyabhavsar1618@gmail.com>
2025-07-30 17:03:48 -0300

    Project Setup Completed

---
+++ Arbitrage1.cpp
@@ -0,0 +1,12 @@
+﻿// Arbitrage1.cpp : Defines the entry point for the application.
+//
+
+#include "Arbitrage1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
